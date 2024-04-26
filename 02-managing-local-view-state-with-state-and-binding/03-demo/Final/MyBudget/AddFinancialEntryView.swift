1d2eaa89077caf0dd4bc07af53e530010a5ff4a2
Rene Cacheaux <rene.cacheaux@gmail.com>
2024-04-26 10:10:57 -0500

    Add Starter and Final projects for video demo in lesson 2.

---
+++ 02-managing-local-view-state-with-state-and-binding/03-demo/Final/MyBudget/AddFinancialEntryView.swift
@@ -0,0 +1,88 @@
+/// Copyright (c) 2024 Kodeco Inc.
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+struct AddFinancialEntryView: View {
+  @Binding var financialEntries: [FinancialEntry]
+  @Binding var showingAddView: Bool
+  @State private var amount: Double = 0
+  @State private var category: String = ""
+  @State private var isExpense = true
+
+  var body: some View {
+    NavigationStack {
+      Form {
+        TextField("Amount", value: $amount, format: .number)
+        TextField("Category", text: $category)
+        Toggle(isOn: $isExpense) {
+          Text("Is Expense")
+        }
+      }
+      .scrollDisabled(true)
+      .navigationTitle("Add Entry")
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItem(placement: .cancellationAction) {
+          Button("Cancel") {
+            showingAddView.toggle()
+          }
+        }
+        ToolbarItem(placement: .confirmationAction) {
+          Button("Save") {
+            let newEntry = FinancialEntry(id: UUID(), amount: amount, category: category, isExpense: isExpense)
+            financialEntries.append(newEntry)
+            showingAddView.toggle()
+          }
+        }
+      }
+    }
+    .onChange(of: amount, initial: false) { oldValue, newValue in
+      print("Amount changed from \(oldValue) to \(newValue)")
+    }
+  }
+}
+
+#Preview {
+  struct AddFinancialEntryViewPreview: View {
+    @State private var financialEntries: [FinancialEntry] = []
+    @State private var showingAddView = true
+
+    var body: some View {
+      AddFinancialEntryView(
+        financialEntries: $financialEntries,
+        showingAddView: $showingAddView)
+    }
+  }
+
+  return AddFinancialEntryViewPreview()
+}
